4aed9f49f0ebe185f3dfa91429d55af726c8e1ab
Alicia Gorton <apersigehl@gmail.com>
2020-10-29 09:36:52 -0700

    finished adding moduels and routes folders

--- models/workout.js
+++ models/workout.js
@@ -3,19 +3,54 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const workoutSchema = new Schema({
-  name: {
-    type: String,
-    trim: true,
-    required: [true, "Enter a name for workout"]
+  day: {
+  type: Date,
+  default: () => new Date()
   },
-  exercises: {
-    type: Schema.Types.ObjectId,
-    ref: "Exercises"
-  },
-//   date: {
-//     type: Date,
-//     default: Date.now
-//   }
+  exercises: [
+    {
+      type: {
+          type: String,
+          trim: true,
+          required: "Enter a type of exercise"
+      },
+      name: {
+          type: String,
+          trim: true,
+          required: "Enter the name of the exercise"
+      },
+      duration: {
+          type: Number,
+          required: "Enter duration of the exercise"
+      },
+      weight: {
+          type: Number
+      },
+      reps: {
+          type: Number
+      },
+      sets: {
+          type: Number
+      },
+      distance: {
+          type: Number
+      }
+    }
+  ]
+},
+//virtuals go here
+{
+  toJSON: {
+    // include any virtual properties when data is requested
+    virtuals: true
+  }
+});
+// adds a dynamically-created property to schema
+workoutSchema.virtual("totalDuration").get(function () {
+// "reduce" array of exercises down to just the sum of their durations
+return this.exercises.reduce((total, exercise) => {
+return total + exercise.duration;
+}, 0);
 });
 
 const Workout = mongoose.model("Workout", workoutSchema);
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,34 +2,50 @@ const router = require("express").Router();
 const Workout = require("../models/workout");
 
 //make a get route @ "/api/workouts"
-
+router.get("/api/workouts", function (req, res){
+  Workout.find()
+  .then(data => {
+    res.join (data)
+  })
+  .catch(err => {
+    res.status(400).join (err)
+  })
+});
 //make a put route @ "/api/workouts/:id"
+router.put("/api/workouts/:id", ({body, params}, res) => {
+  Workout.findByIdAndUpdate(
+    params.id,
+    {$push: {exercises: body} },
+    {new: true, runValidators: true }
+  )
+  .then(data => 
+    res.join (data))
+  .catch(err => {
+    res.status(400).join (err)
+  })
+});
 
 //make a post route @ "/api/workouts"
+router.post("/api/workouts", function (req, res){
+  Workout.create({})
+  .then(data => 
+    res.join (data))
+  .catch(err => {
+    console.log("err", err)
+    res.status(400).join (err)
+  })
+});
 
 //make a get route @ "/api/workouts/range"
+router.get("/api/workouts/range", function (req, res){
+  Workout.find()
+  .then(data => {
+    res.join (data)
+  })
+  .catch(err => {
+    res.status(400).join (err)
+  })
+});v
 
-
-router.post("/api/transaction", ({ body }, res) => {
-  Transaction.create(body)
-    .then(dbTransaction => {
-      res.json(dbTransaction);
-    })
-    .catch(err => {
-      res.status(400).json(err);
-    });
-});
-
-
-router.get("/api/transaction", (req, res) => {
-  Transaction.find({})
-    
-    .then(dbTransaction => {
-      res.json(dbTransaction);
-    })
-    .catch(err => {
-      res.status(400).json(err);
-    });
-});
 
 module.exports = router;
